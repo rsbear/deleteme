3fc08f01a28617c6136c35d0d2f0f260bc5ec2ed
rs <ross@sundayscaries.run>
2025-07-20 01:51:26 -0400

    dfa

--- mini-svelte-ts/harbor.ts
+++ mini-svelte-ts/harbor.ts
@@ -1,5 +1,5 @@
 import { mount as svelteMount } from "svelte";
-import App from "./App.svelte" with { type: "module" };
+import App from "./App.svelte" with { type: "bytes" };
 
 export const harborMini = "svelte";
 export function harborMount(target: HTMLElement, harborApi: any) {
