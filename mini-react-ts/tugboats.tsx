8f382c78dd8ab6e7ae9dfa2d54c6da9a75a82135
rsbear <hellorosss@gmail.com>
2025-09-08 14:24:02 -0400

    man

--- mini-react-ts/tugboats.tsx
+++ mini-react-ts/tugboats.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
+import * as input from '@tugboats/core'
 
 // The App now receives the Harbor API as a prop, allowing it to interact
 // with the main application shell.
 function App() {
+  useEffect(() => {
+    async function yup() {
+      const what = await input.value()
+      console.log("what", what)
+    }
+yup()
+  }, [])
 	return (
 		<div className="bg-gray-800/50 p-6 rounded-lg border border-gray-700 animate-fade-in">
 			<h1 className="text-2xl font-bold text-white">
