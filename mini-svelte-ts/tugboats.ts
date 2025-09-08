7f43cfe811f3338a0e3e9b63a8a9cf5768e457c2
rsbear <hellorosss@gmail.com>
2025-09-08 14:13:36 -0400

    ignoreme

--- mini-svelte-ts/tugboats.ts
+++ mini-svelte-ts/tugboats.ts
@@ -1,7 +1,7 @@
 import { mount as svelteMount } from "svelte";
 import App from "./App.svelte";
 
-export const harborMini = "svelte";
-export function harborMount(target: HTMLElement, harborApi: any) {
+export const tugboatsMini = "svelte";
+export function tugboatsMount(target: HTMLElement, harborApi: any) {
   svelteMount(App, { target, props: { yeye: "yeyeye" } });
 }
